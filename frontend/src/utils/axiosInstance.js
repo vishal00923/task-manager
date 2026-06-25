ecf473a86fbf27d9a22cd041ac49286a138808b1
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-06-25 17:33:03 +0530

    feat(utils): complete axiosInstance, apiPaths

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+import { BASE_URL } from './apiPaths';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('token');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Handle common errors globally
+    if (error.res) {
+      if (error.res.status === 401) {
+        // Redierct to login page
+        window.location.href = '/login';
+      } else if (error.res.status === 500) {
+        console.error('Server error, please try again later.');
+      }
+    } else if (error.code === 'ECONNABORTED') {
+      console.error('Request timeout, please try again.');
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
