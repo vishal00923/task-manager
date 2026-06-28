3077823f2552e2c87a756c1fdc79485e17f734ce
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-06-28 19:12:29 +0530

    feat(Admin): Admin Dashboard implementation

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { UserContext } from '../../context/userContext';
+
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  useUserAuth();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+
+  return <DashboardLayout activeMenu='Dashboard'>Dashboard</DashboardLayout>;
 };
 
 export default Dashboard;
