5e812fd1b51c00dd313eaaaba64bff9017fffac5
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-07-11 22:20:00 +0530

    Cards: TaskCard component update

--- frontend/src/components/Cards/TaskCard.jsx
+++ frontend/src/components/Cards/TaskCard.jsx
@@ -40,8 +40,11 @@ const TaskCard = ({
     }
   };
   return (
-    <div className='' onClick={onClick}>
-      <div className=''>
+    <div
+      className='bg-white rounded-xl py-4 shadow-md shadow-gray-100 border border-gray-200/50 cursor-pointer'
+      onClick={onClick}
+    >
+      <div className='flex items-end gap-3 px-4'>
         <div className={`text-[11px] font-medium ${getStatusTagColor()} px-4 py-0.5 rounded`}>
           {status}
         </div>
@@ -54,13 +57,15 @@ const TaskCard = ({
       <div
         className={`px-4 border-l-[3px] ${status === 'In Progress' ? 'border-cyan-500' : status === 'Completed' ? 'border-indigo-500' : 'border-violet-500'}`}
       >
-        <p className=''>{title}</p>
+        <p className='text-sm font-medium text-gray-800 mt-4 line-clamp-2'>{title}</p>
 
-        <p className=''>{description}</p>
+        <p className='text-xs font-medium text-gray-500 mt-1.5 line-clamp-2 leading-[18px]'>
+          {description}
+        </p>
 
-        <p className=''>
+        <p className='text-[13px] text-gray-700/80 font-medium mt-2 mb-2 leading-[18px]'>
           Task Done:{' '}
-          <span className=''>
+          <span className='font-semibold text-gray-700'>
             {completedTodoCount} / {todoChecklist.length || 0}
           </span>
         </p>
@@ -68,19 +73,34 @@ const TaskCard = ({
         <Progress progress={progress} status={status} />
       </div>
 
-      <div className=''>
-        <div className=''>
+      <div className='px-4'>
+        <div className='flex items-center justify-between my-1'>
           <div>
-            <label className=''>Start Date</label>
+            <label className='text-xs text-gray-500'>Start Date</label>
 
-            <p className=''>{moment(createdAt).format('Do MMM YYYY')}</p>
+            <p className='text-[13px] font-medium text-gray-900'>
+              {moment(createdAt).format('Do MMM YYYY')}
+            </p>
           </div>
 
           <div>
-            <label className=''>Due Date</label>
+            <label className='text-xs text-gray-500'>Due Date</label>
 
-            <p className=''>{moment(dueDate).format('Do MMM YYYY')}</p>
+            <p className='text-[13px] font-medium text-gray-900'>
+              {moment(dueDate).format('Do MMM YYYY')}
+            </p>
           </div>
+        </div>
+
+        <div className='flex items-center justify-between mt-3'>
+          <AvatarGroup avatars={assignedTo || []} />
+
+          {attachmentCount > 0 && (
+            <div className='flex items-center gap-2 bg-blue-50 px-2.5 py-1.5 rounded-lg'>
+              <LuPaperclip className='text-primary' />
+              <span className='text-xs text-gray-900'>{attachmentCount}</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
