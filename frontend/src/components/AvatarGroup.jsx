5fd0bb5d4c45e0d0b470374de927eb2e963572dd
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-07-11 22:19:00 +0530

    deafult avatar fallback implementation

--- frontend/src/components/AvatarGroup.jsx
+++ frontend/src/components/AvatarGroup.jsx
@@ -1,10 +1,12 @@
+import DEFAULT_PROFILE from '../assets/images/default_profile.png';
+
 const AvatarGroup = ({ avatars, maxVisible = 3 }) => {
   return (
     <div className='flex items-center'>
       {avatars.slice(0, maxVisible).map((avatar, index) => (
         <img
           key={index}
-          src={avatar}
+          src={avatar || DEFAULT_PROFILE}
           alt={`Avatar ${index}`}
           className='w-9 h-9 rounded-full border-2 border-white -ml-3 first:ml-0'
         />
