79de14798cd077008065047274c57a12ba1c2c77
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-07-12 17:24:12 +0530

    feat(User): UserDashboard page implementation

---
+++ frontend/src/pages/User/UserDashboard.jsx
@@ -0,0 +1,173 @@
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LuArrowRight } from 'react-icons/lu';
+
+import moment from 'moment';
+
+import { API_PATHS } from '../../utils/apiPaths';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { UserContext } from '../../context/userContext';
+import { axiosInstance } from '../../utils/axiosInstance';
+import { addThousandsSeparator, capitalizeName } from '../../utils/helper';
+
+import InfoCard from '../../components/Cards/InfoCard';
+import CustomPieChart from '../../components/Charts/CustomPieChart';
+import CustomBarChart from '../../components/Charts/CustomBarChart';
+
+import TaskListTable from '../../components/TaskListTable';
+
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+
+const COLORS = ['#8D51FF', '#00B8DB', '#7BCE00'];
+
+const UserDashboard = () => {
+  useUserAuth();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      {
+        status: 'Pending',
+        count: taskDistribution?.Pending || 0,
+      },
+      {
+        status: 'In Progress',
+        count: taskDistribution?.InProgress || 0,
+      },
+      {
+        status: 'Completed',
+        count: taskDistribution?.Completed || 0,
+      },
+    ];
+
+    setPieChartData(taskDistributionData);
+
+    const taskPriorityLevelsData = [
+      {
+        priority: 'Low',
+        count: taskPriorityLevels?.Low || 0,
+      },
+      {
+        priority: 'Medium',
+        count: taskPriorityLevels?.Medium || 0,
+      },
+      {
+        priority: 'High',
+        count: taskPriorityLevels?.High || 0,
+      },
+    ];
+
+    setBarChartData(taskPriorityLevelsData);
+  };
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_USER_DASHBOARD_DATA);
+
+      if (response.data) {
+        setDashboardData(response.data);
+        prepareChartData(response.data?.charts || null);
+      }
+    } catch (error) {
+      console.log('Error fetching users:', error);
+    }
+  };
+
+  const onSeeMore = () => {
+    navigate('/user/tasks');
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {};
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu='Dashboard'>
+      <div className='card my-5'>
+        <div>
+          <div className='col-span-3'>
+            <h2 className='text-xl md:text-2xl'>Good Morning! {capitalizeName(user?.name)}</h2>
+            <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
+              {moment().format('dddd Do MMM YYYY')}
+            </p>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5'>
+          <InfoCard
+            color='bg-primary'
+            label='Total Tasks'
+            value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.All || 0)}
+          />
+
+          <InfoCard
+            color='bg-violet-500'
+            label='Pending Tasks'
+            value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Pending || 0)}
+          />
+
+          <InfoCard
+            color='bg-cyan-500'
+            label='In Progress Tasks'
+            value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.InProgress || 0)}
+          />
+
+          <InfoCard
+            color='bg-lime-500'
+            label='Completed Tasks'
+            value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Completed || 0)}
+          />
+        </div>
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6'>
+        <div>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='font-medium'>Task Distribution</h5>
+            </div>
+
+            <CustomPieChart data={pieChartData} colors={COLORS} />
+          </div>
+        </div>
+
+        <div>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='font-medium'>Task Priority Levels</h5>
+            </div>
+
+            <CustomBarChart data={barChartData} />
+          </div>
+        </div>
+
+        <div className='md:col-span-2'>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='text-lg'>Recent Tasks</h5>
+
+              <button className='card-btn' onClick={onSeeMore}>
+                See All <LuArrowRight className='text-base' />
+              </button>
+            </div>
+
+            <TaskListTable tableData={dashboardData?.recentTasks || []} />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default UserDashboard;
