c283d66a0affa1a614c46862a3fa3d34655fbd5f
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-07-13 15:43:43 +0530

    APP: routes, context & toast notification implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,71 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 import PrivateRoute from './routes/PrivateRoute';
 
 import Login from './pages/Auth/Login';
 import SignUp from './pages/Auth/SignUp';
 import Dashboard from './pages/Admin/Dashboard';
-import ManageTasks from './pages/Admin/ManageTasks';
 import CreateTask from './pages/Admin/CreateTask';
+import ManageTasks from './pages/Admin/ManageTasks';
 import ManageUsers from './pages/Admin/ManageUsers';
 
 import MyTasks from './pages/User/MyTasks';
-import UserDashboad from './pages/User/Dashboard';
 import TaskDetails from './pages/User/TaskDetails';
+import UserDashboad from './pages/User/UserDashboard';
+
+import UserProvider, { UserContext } from './context/userContext';
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
 
-          {/* ADMIN ROUTES */}
-          <Route element={<PrivateRoute allwedRoles={['admin']} />} />
-          <Route path='/admin/dashboard' element={<Dashboard />} />
-          <Route path='/admin/tasks' element={<ManageTasks />} />
-          <Route path='/admin/create-task' element={<CreateTask />} />
-          <Route path='/admin/users' element={<ManageUsers />} />
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allwedRoles={['admin']} />} />
+            <Route path='/admin/dashboard' element={<Dashboard />} />
+            <Route path='/admin/tasks' element={<ManageTasks />} />
+            <Route path='/admin/create-task' element={<CreateTask />} />
+            <Route path='/admin/users' element={<ManageUsers />} />
 
-          {/* USER ROUTES */}
-          <Route element={<PrivateRoute allwedRoles={['admin']} />} />
-          <Route path='/user/dashboard' element={<UserDashboad />} />
-          <Route path='/user/my-tasks' element={<MyTasks />} />
-          <Route path='/user/task-details/:id' element={<TaskDetails />} />
-        </Routes>
-      </Router>
-    </div>
+            {/* User Routes */}
+            <Route element={<PrivateRoute allwedRoles={['admin']} />} />
+            <Route path='/user/dashboard' element={<UserDashboad />} />
+            <Route path='/user/tasks' element={<MyTasks />} />
+            <Route path='/user/task-details/:id' element={<TaskDetails />} />
+
+            {/* Default Route */}
+            <Route path='/' element={<Root />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className: '',
+          style: { fontSize: '13px' },
+        }}
+      />
+    </UserProvider>
   );
 };
 
 export default App;
+
+const Root = () => {
+  const { user, loading } = useContext(UserContext);
+
+  if (loading) return <Outlet />;
+  if (!user) return <Navigate to='/login' />;
+
+  return user.role === 'admin' ? (
+    <Navigate to='/admin/dashboard' />
+  ) : (
+    <Navigate to='/user/dashboard' />
+  );
+};
