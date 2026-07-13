9c4395412650ff8b1de84c478406a3545f3135b4
Vishal Chaurasia <vishalbg.mz@gmail.com>
2026-07-13 15:46:02 +0530

    helper functions update

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -2,3 +2,18 @@ export const validateEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 };
+
+export const capitalizeName = (name) => {
+  if (name) {
+    return name.toLowerCase().replace(/\b(\w)/g, (ch) => ch.toUpperCase());
+  }
+};
+
+export const addThousandsSeparator = (num) => {
+  if (num === null || isNaN(num)) return '';
+
+  const [integerPart, fractionalPart] = num.toString().split('.');
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  return fractionalPart ? `${formattedInteger}.${fractionalPart}` : formattedInteger;
+};
